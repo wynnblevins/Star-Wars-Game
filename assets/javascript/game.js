af79b7d52f71bdb3830801e3999794230ba2f8c2
Wynn Blevins <wynnblevins@yahoo.com>
2018-02-20 12:48:58 -0500

    game.js now figures out which character was clicked based on click index.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,22 @@
 (function () {
     'use strict';
 
+    var selectedChar = '';
     var $characterPics = $('.character-picture');
+    
     $characterPics.click(function () {
-        console.log($('.character-picture').index(this));   
+        var ndx = $('.character-picture').index(this);
+        
+        if (ndx === 0) {
+            selectedChar = 'Luke Skywalker';
+        } else if (ndx === 1) {
+            selectedChar = 'The Emperor';
+        } else if (ndx === 2) {
+            selectedChar = 'Obi Wan Kenobi';
+        } else if (ndx === 3) {
+            selectedChar = 'Darth Vader';
+        }
+
+        console.log(selectedChar);
     });
 })();
