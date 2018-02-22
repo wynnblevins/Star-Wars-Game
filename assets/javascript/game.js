8de1bba169b2dbc26ecf22fa5d617d86dd19e743
Wynn Blevins <wynnblevins@yahoo.com>
2018-02-22 14:19:53 -0500

    Character name and hp are now displayed inside character boxes.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,23 @@
         var $defenderSection = $('#defender-section');
         var selectionNdx = null;
         
+        // initialize character array
+        var characters = [
+            { name: 'Luke Skywalker', hp: 120, isPlayerCharacter: false },
+            { name: 'The Emperor', hp: 150, isPlayerCharacter: false },
+            { name: 'Obi Wan Kenobi', hp: 130, isPlayerCharacter: false },
+            { name: 'Darth Vader', hp: 140, isPlayerCharacter: false }
+        ];
+
+        // display character name
+        $('.character-name-text').each(function (index, value) {
+            $(value).text(characters[index].name);        
+        });
+
+        // display character hp
+        $('.character-hp').each(function (index, value) {
+            $(value).text(characters[index].hp);        
+        });
 
         $unselected.click(function () {
             var selectedCharacter = this;
@@ -25,7 +42,7 @@
                     $enemySection.append($enemy);
                 }
             }
-
+    
             // remove unselected css class and unselected event handler
             $charBox.removeClass('unselected');
             $('.unselected').off();
@@ -33,7 +50,6 @@
 
         // user has clicked on an enemy character
         $($enemySection).delegate(".available-enemy", "click", function(){
-            
             var $charBox = $('.available-enemy');
             selectionNdx = $charBox.index(this);
             var $selectedChild = $($enemySection).children().eq(selectionNdx);
@@ -44,7 +60,7 @@
         });
 
         $('#attack-button').click(function () {
-            console.log('hello world!');    
+            
         });
     });
     
