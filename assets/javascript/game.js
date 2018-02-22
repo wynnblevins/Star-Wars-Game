cde9c52387f1ebf3ee6fa2c05b0f1c9beca6a5aa
Wynn Blevins <wynnblevins@yahoo.com>
2018-02-22 00:05:37 -0500

    User can now pick a character, then, from the remaining characters, pick an enemy.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,46 @@
 (function () {
     'use strict';
 
-    var selectedChar = '';
-    var $characterBoxes = $('.character-box');
-    
-    $characterBoxes.click(function () {
-        var selectedCharacter = this;
-        var $charBox = $('.character-box');
-        var selectionNdx = $charBox.index(this);
-        var characterSelector = 'ul#your-character-list';
-        var $selectedChild= $(characterSelector).children().eq(selectionNdx);
-        var numOfCharacters = $(characterSelector).children().length;
+    $(document).ready(function () {
+        var selectedChar = '';
+        var $unselected = $('.unselected');
+        var $enemySection = $('#enemy-list');
+        var $targetEnemy = null, $enemy = null; 
+        var $defenderSection = $('#defender-section');
+        var selectionNdx = null;
 
-        // remove all characters except selected character from character choice section
-        for (var currCharacterNdx = 0; currCharacterNdx <= numOfCharacters; ++currCharacterNdx) {
-            if (currCharacterNdx !== selectionNdx) {
-                $charBox.eq(currCharacterNdx).remove();    
+        $unselected.click(function () {
+            var selectedCharacter = this;
+            var $charBox = $('.unselected');
+            selectionNdx = $charBox.index(this);
+            var characterSelector = 'div#character-list';
+            var $selectedChild = $(characterSelector).children().eq(selectionNdx);
+            var numOfCharacters = $(characterSelector).children().length;
+            
+            for (var currCharacterNdx = 0; currCharacterNdx < numOfCharacters; ++currCharacterNdx) {
+                if (currCharacterNdx !== selectionNdx) {
+                    $enemy = $charBox.eq(currCharacterNdx).detach();
+                    $enemy.addClass('available-enemy');
+                    $enemySection.append($enemy);
+                }
             }
-        }
+
+            // remove unselected css class and unselected event handler
+            $charBox.removeClass('unselected');
+            $('.unselected').off();
+        });
+
+        // user has clicked on an enemy character
+        $($enemySection).delegate(".available-enemy", "click", function(){
+            
+            var $charBox = $('.available-enemy');
+            selectionNdx = $charBox.index(this);
+            var $selectedChild = $($enemySection).children().eq(selectionNdx);
+            $selectedChild.detach();
+            $selectedChild.removeClass('available-enemy');
+            $selectedChild.addClass('defender');
+            $($defenderSection).append($selectedChild);
+        });
     });
+    
 })();
