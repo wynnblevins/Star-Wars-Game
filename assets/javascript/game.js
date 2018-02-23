37b20cc25d1b0978b5b567d397e077d8cd2d408a
Wynn Blevins <wynnblevins@yahoo.com>
2018-02-23 01:03:18 -0500

    Saving progress so I can pull

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,16 +5,18 @@
         var selectedChar = '';
         var $unselected = $('.unselected');
         var $enemySection = $('#enemy-list');
-        var $targetEnemy = null, $enemy = null; 
+        var $targetEnemy = null, $enemy = null, $currentEnemy = null; 
         var $defenderSection = $('#defender-section');
         var selectionNdx = null;
-        
+        var baseAttackPower = 6;
+        var $playerCharacter, $enemy = null;
+
         // initialize character array
         var characters = [
-            { name: 'Luke Skywalker', hp: 120, isPlayerCharacter: false },
-            { name: 'The Emperor', hp: 150, isPlayerCharacter: false },
-            { name: 'Obi Wan Kenobi', hp: 130, isPlayerCharacter: false },
-            { name: 'Darth Vader', hp: 140, isPlayerCharacter: false }
+            { name: 'Luke Skywalker', hp: 120, isPlayerCharacter: false, attackPower: 6, counterAttackPower: 6 },
+            { name: 'The Emperor', hp: 150, isPlayerCharacter: false, attackPower: 20, counterAttackPower: 12 },
+            { name: 'Obi Wan Kenobi', hp: 130, isPlayerCharacter: false, attackPower: 10, counterAttackPower: 8 },
+            { name: 'Darth Vader', hp: 140, isPlayerCharacter: false, attackPower: 15, counterAttackPower: 10 }
         ];
 
         // display character name
@@ -34,12 +36,16 @@
             var characterSelector = 'div#character-list';
             var $selectedChild = $(characterSelector).children().eq(selectionNdx);
             var numOfCharacters = $(characterSelector).children().length;
-            
+
             for (var currCharacterNdx = 0; currCharacterNdx < numOfCharacters; ++currCharacterNdx) {
                 if (currCharacterNdx !== selectionNdx) {
                     $enemy = $charBox.eq(currCharacterNdx).detach();
                     $enemy.addClass('available-enemy');
                     $enemySection.append($enemy);
+                } else {
+                    $playerCharacter = $(characterSelector).children().eq(selectionNdx);
+                    $playerCharacter.characterObject = characters[selectionNdx];
+                    $playerCharacter.isPlayerCharacter = true;  
                 }
             }
     
@@ -52,15 +58,25 @@
         $($enemySection).delegate(".available-enemy", "click", function(){
             var $charBox = $('.available-enemy');
             selectionNdx = $charBox.index(this);
-            var $selectedChild = $($enemySection).children().eq(selectionNdx);
-            $selectedChild.detach();
-            $selectedChild.removeClass('available-enemy');
-            $selectedChild.addClass('defender');
-            $($defenderSection).append($selectedChild);
+            $enemy = $($enemySection).children().eq(selectionNdx);
+            $enemy.characterObject = characters[selectionNdx];
+            $enemy.detach();
+            $enemy.removeClass('available-enemy');
+            $enemy.addClass('defender');
+            $($defenderSection).append($enemy);
         });
 
-        $('#attack-button').click(function () {
+        $('#attack-button').click(function () {            
+            // subtract attack power from enemy hp
             
+            // double players attack power
+            $playerCharacter.characterObject.attackPower *= 2;
+
+            // subtract enemy's counter attack power from player's hp
+            $playerCharacter.characterObject.hp -= $enemy.characterObject.counterAttackPower;
+
+            alert('Your HP: ' + $playerCharacter.characterObject.hp);
+            alert('Enemy HP: ' + $enemy.characterObject.hp);
         });
     });
     
